e4834ce4a00eff5380af10e6ae0108847a02d9b1
janzheng <janeazy@gmail.com>
2022-02-12 10:34:13 -0500

    spinny spin

--- src/lib/components/Ngl.svelte
+++ src/lib/components/Ngl.svelte
@@ -14,23 +14,24 @@
     if (browser) {
       ngl = (await import('ngl'))
       stage = new ngl.Stage("viewport")
+
+      console.log('::: loading ngl::', ngl)
+      stage.setParameters({backgroundColor: bgColor, clipFar: 1000})
+      stage.setSpin(true)
+      stage.loadFile("/gp47_relaxed.pdb", {defaultRepresentation: true});
+      // stage.loadFile("/gp47_relaxed.pdb");
+
+      // stage.loadFile("/gp47_relaxed.pdb").then(function (o) {
+      //   o.addRepresentation("cartoon");
+      //   var duration = 1000;  // optional duration for animation, defaults to zero
+      //   o.autoView(duration);  // focus on the whole structure
+      // });
+
+      console.log('stage:', stage)
     }
   })
 
-  $: if(browser && ngl) {
-    console.log('::: loading ngl::', ngl)
-    stage.setParameters({backgroundColor: bgColor, clipFar: 1000})
-    stage.setSpin(true)
-    // stage.loadFile("/gp47_relaxed.pdb", {defaultRepresentation: true});
-    stage.loadFile("/gp47_relaxed.pdb");
-
-    // stage.loadFile("/gp47_relaxed.pdb").then(function (o) {
-    //   o.addRepresentation("cartoon");
-    //   var duration = 1000;  // optional duration for animation, defaults to zero
-    //   o.autoView(duration);  // focus on the whole structure
-    // });
-
-    console.log('stage:', stage)
-  }
+  // $: if(browser && ngl) {
+  // }
   
 </script>
