1a0c1865c38933d3950cb8c4da9eb0041000e6b6
janzheng <janeazy@gmail.com>
2022-03-05 12:58:44 +1100

    nav elems

--- src/lib/components/shared/MenuDesktop.svelte
+++ src/lib/components/shared/MenuDesktop.svelte
@@ -9,28 +9,22 @@
   // prefetchRoutes() // Loads ALL routes in the background
 </script>
 
-<!-- <header class="menu-desktop hidden sm:block relative max-w-7xl mx-auto max-w-7xl md:flex items-center leading-6"> -->
-<header class="menu-desktop sm:block sm:visible sm:relative max-w-[14rem] md:flex items-center leading-6">
-  <div class="mr-4">
-    <a sveltekit:prefetch class="decoration-transparent hover:fill-_hover" href='/'>
-      <img src={logo} alt="SvelteKit" />
-    </a>
+
+<header class="menu-desktop relative flex items-center justify-between leading-6">
+  <div class="">
+    <div class="max-w-[12rem] mr-4">
+      <a sveltekit:prefetch class="decoration-transparent hover:fill-_hover" href='/'>
+        <img src={logo} alt="SvelteKit" />
+      </a>
+    </div>
   </div>
-
-  <!-- <nav class="menu-items relative flex items-center flex-wrap gap-4 justify-between w-full px-8 pr-8"> -->
-    <!-- <h2 class="font-light p-0">Decentralized scientific research</h2> -->
-    <!-- <ul class="menu-items-left text-lg md:space-x-4 md:flex flex-row place-content-end pr-8 list-none">
-      <li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-      <li class:active={$page.url.pathname === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
-      <li class:active={$page.url.pathname === '/todos'}><a sveltekit:prefetch href="/todos">Todos</a></li>
-    </ul> -->
-    <!-- <ul class="menu-items-right text-lg md:space-x-4 md:flex flex-row items-baseline place-content-end mt-8 md:mt-0">
-      <a href="/">Right side item</a>
-    </ul> -->
-  <!-- </nav> -->
-
   <div class="corner">
-    <!-- TODO put something else here? github link? -->
+    <a sveltekit:prefetch class="text-xl ml-2 decoration-transparent hover:fill-_hover" href='https://github.com/labdao'>
+      Github
+    </a>
+    <a sveltekit:prefetch class="text-xl ml-2 decoration-transparent hover:fill-_hover" href='https://twitter.com/lab_dao'>
+      @lab_dao
+    </a>
   </div>
 </header>
 
