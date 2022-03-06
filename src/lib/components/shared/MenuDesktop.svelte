65c989e878b8461c442e6269e23b0cc22945e27e
Jesse Evers <jesse@jesseevers.com>
2022-03-06 17:36:22 -0500

    Add GitHub and Twitter logos, rename Twitter nav link

--- src/lib/components/shared/MenuDesktop.svelte
+++ src/lib/components/shared/MenuDesktop.svelte
@@ -1,8 +1,8 @@
-
-
 <!-- desktop nav -->
 <script>
   export let logo
+  import githubLogo from '../../../../static/images/github-logo.svg'
+  import twitterLogo from '../../../../static/images/twitter-logo.svg'
   // import { prefetch, prefetchRoutes } from '$app/navigation'
 
   // prefetch('/blog') // Loads the blog page in the background
@@ -20,10 +20,10 @@
   </div>
   <div class="corner">
     <a sveltekit:prefetch class="text-xl ml-2 decoration-transparent hover:fill-_hover" href='https://github.com/labdao'>
-      Github
+      <img src={githubLogo} class="inline w-6 h-6" alt="Github logo" />&nbsp;<div class="hidden md:inline">Github</div>
     </a>
     <a sveltekit:prefetch class="text-xl ml-2 decoration-transparent hover:fill-_hover" href='https://twitter.com/lab_dao'>
-      @lab_dao
+      <img src={twitterLogo} class="inline w-6 h-6" alt="Twitter logo" />&nbsp;<div class="hidden md:inline">Twitter</div>
     </a>
   </div>
 </header>
